bef8c929dfd39eed55d1ea3a49b064030e04ab0b
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-12 17:49:51 -0500

    Dom interaction

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-import fetch from "node-fetch"
-const API_KEY = ''
-var country = 'Mexico'
-
-function fetchWeather(){
+function fetchWeather() {
+    const API_KEY = ''
+    var country = 'Mexico'
     fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=${country}&units=metric`)
     .then(response => response.json())
     .then(body =>{
+        var country = body.name
         var temp = body.main.temp
         var temp_min = body.main.temp_min
         var temp_max = body.main.temp_max
-        var humidity = body.main.humidity // Percent
-        console.log(temp)      
+        // var humidity = body.main.humidity // Percent
+        const tempToHTML = document.getElementById("temperature").innerText = `${temp}°`      
+        const tempMinToHTML = document.getElementById("temperatureMax").innerText = `${temp_max}°`      
+        const tempMaxToHTML = document.getElementById("temperatureMin").innerText = `${temp_min}°` 
+        const countryToHTML = document.getElementById("country").innerText = `${country}` 
     })
 }
-
-fetchWeather()
