cf0710e0b4809d24f98af54046071f2647c97988
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-12 22:26:49 -0500

    Search by input

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function fetchWeather() {
     const API_KEY = ''
-    var country = 'Mexico'
+    
+    var country = document.getElementById("country").value
     fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=${country}&units=metric`)
     .then(response => response.json())
     .then(body =>{
-        var country = body.name
         var temp = body.main.temp
         var temp_min = body.main.temp_min
         var temp_max = body.main.temp_max
-        // var humidity = body.main.humidity // Percent
+
         const tempToHTML = document.getElementById("temperature").innerText = `${temp}°`      
-        const tempMinToHTML = document.getElementById("temperatureMax").innerText = `${temp_max}°`      
-        const tempMaxToHTML = document.getElementById("temperatureMin").innerText = `${temp_min}°` 
-        const countryToHTML = document.getElementById("country").innerText = `${country}` 
+        const tempMinToHTML = document.getElementById("temperatureMax").innerText = `${temp_max}° max`      
+        const tempMaxToHTML = document.getElementById("temperatureMin").innerText = `${temp_min}° min` 
     })
 }
